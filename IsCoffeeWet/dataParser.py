1b6a1fe0497c55fd0a454b653915a9d75bb75d9f
Andres Ramirez Quiros <andrew.rq+gitlab@gmail.com>
2020-08-17 19:20:02 -0600

    Remodelde the dataParser
    - Refactor name of functions
    - Remove default values
    - Separate the mergeDateTime function for better readability
    -  NaN values are now interpolated instead of removed
    - Remove warnings as they where not descriptive enough
    -  Remove the prints

--- IsCoffeeWet/dataParser.py
+++ IsCoffeeWet/dataParser.py
@@ -2,143 +2,114 @@ import pandas as pd
 import numpy as np
 import warnings
 
+# IMPORTANT: The dataset used mustn't contain trailing whitespaces
 
-def cleanDataset(dataset, removeRows=[], nullValue="", removeColumns=[]):
+
+def mergeDateTime(dataset, dateName, timeName):
     """
-    Removes missing or empty values that can generate error in the parse 
-    of the dataset
+    Merges the Date column and the Time column into a
+    single one, used for indexing the dataframe with
+    pandas.
 
-    Returns a dataset without "damaged" values that could hurt a convertion
-    of a `dtype` in a column of the dataset
+    Parameters
+    ----------
+    - dataset: pd.DataFrame.
+        Dataset to merge the date and time.
+    - dateName: string.
+        Name of the column with the dates. Example:
+        >>> "Date"
+    - timeName: string.
+        Name of the column with the times. Example:
+        >>> "Time"
+
+    Returns
+    -------
+    - dataset : pd.DataFrame.
+        Dataset with a datetime column as its main
+        index. Previous columns with date and time
+        removed.
+    """
+
+    # Unifies "Date" and "Time" in a "datetime64[ns]"
+    dataset["Datetime"] = pd.to_datetime(dataset[dateName] +
+                                         ' ' + dataset[timeName])
+    # Sets the datetime as the index of the DataFrame
+    dataset = dataset.set_index('Datetime')
+    # Drops the "Time" columns as it has been combine into "Date"
+    dataset = dataset.drop([dateName, timeName], axis=1)
+
+    return dataset
+
+
+def cleanDataset(dataset, completeRows, nullValue):
+    """
+    Clears a dataset of all the damage values
+
+    Returns a dataset without "damaged" values that could hurt 
+    a convertion of a `dtype` in a column of the dataset
 
     Parameters
     ----------
     - dataset: pd.DataFrame.
         Dataset to clean.
-    - removeRows: list or tuple of strings.
-        Columns where to search the null values.
-        Wrong name will result in a failed removal.
-        Default value is an empty list.
+    - completeRows: list of strings.
+        Columns where to search the null values. Example:
+        >>> ["Temp Out", "Leaf Wet 1"]
     - nullValue:  object, generally string or int.
-        Value to search and remove.
-        Default value is an empty string.
-    - removeColumns: list or tuple of strings.
-        Group of columns to remove. Wrong names will
-        result in a failed removal. Default value is 
-        an empty list.
+        Value to search and remove. Example:
+        >>> "---"
 
     Returns
     -------
     - dataset : pd.DataFrame.
-        Dataset with entire columns remove and/or
-        rows with incomplete data removed.
+        Dataset with entire dataset "repaired"
+        (not null values other than NaNs)
     """
-    print()  # ! Print
-    # Removes the columns with almost all missing values
-    displayNumber = 0  # Iterator to print
-    if removeColumns:  # Checks if empty
-        print("Column removal started...")  # ! Print
-        for columnName in removeColumns:  # Remove
-            try:
-                dataset = dataset.drop(columnName, axis=1)
-                displayNumber += 1
-            except (KeyError):
-                np.mean()
-                print("Column doesn't exist:", columnName)
-
-    print("Columns removed:", displayNumber)
-
-    # Sets al the missing values (represented as nullValue) to NaN in the
-    # selected columns
-    displayNumber = dataset.size  # Iterator to print
-    if removeRows:
-        print("Rows removal started...")  # ! Print
-        for rowName in removeRows:
-            try:
-                dataset = dataset.replace({rowName: nullValue}, np.NaN)
-            except (KeyError):
-                print("Column doesn't exist:", columnName)
-
-    # Removes the NaN values in the rows
-    dataset = dataset.dropna(axis=0, how="any")
-
-    print("Rows removed:", displayNumber-dataset.size)
+    for rowName in completeRows:
+        # Replaces nullValue to NaN
+        dataset[rowName] = dataset[rowName].replace(nullValue, np.NaN)
 
     return dataset
 
 
-def setDataTypes(dataset, mergeDateTime=[], nameFormat=[]):
+def convertNumeric(dataset, columnAndType):
     """
-    Reorders the dataset by merging Date & Time into a 
-    pd.datetime64[ns]. It also sets the types of the 
-    rest of the columns according to the `dtype` passed.
+    Sets the type of a column according to the given pair.
+    Use numeric values only (int, float, etc.) or it will fail.
+    If the dataset is not clean (other values that are not NaN),
+    the conversion will fail.
 
     Returns a dataset with the column casted to the desired
-    values for easier manipulation
+    values for easier manipulation.
 
     Parameters
     ----------
     - dataset: pd.DataFrame.
         Dataset to change the `dtype`.
-    - mergeDateTime: tuple of strings.
-        A two strings tuple that has in the first position
-        the name of the column of the dates. In the second 
-        position, the name of the time. Extra parameters will
-        be ignored and one parameter will result in a failed
-        conversion. By default, its an empty list
-    - nameFormat: list of tuples.
+    - columnAndFormat: list of tuples.
         Each tuple is made of 2 parameters: 
-        the name (string) of the column and the `dtype` in a string 
-        to be converted. Wrong name or incorrect casting will result
-        in a failed conversion. By default is an empty list. Example: 
-        >>> [("Temp Out", "float32")]
+        The name (string) of the column and the `dtype` as a string 
+        to be converted. Example: 
+        >>> [("Temp Out", "float32"), ("Leaf Wet 1", "int32")]
 
     Returns
     -------
     - dataset : pd.DataFrame.
-        Dataset with entire columns remove and/or
-        rows with incomplete data removed.
+        Dataset with columns type changed and missing
+        values interpolated.
     """
-    print()  # ! Print
-    if mergeDateTime:
-        try:
-            print("Date and Time merge started...")  # ! Print
-            # Unifies "Date" and "Time" in a "datetime64[ns]"
-            dataset["Datetime"] = pd.to_datetime(dataset[mergeDateTime[0]] +
-                                                 ' ' + dataset[mergeDateTime[1]])
-            # Sets the datetime as the index of the DataFrame
-            dataset = dataset.set_index('Datetime')
-            # Drops the "Time" columns as it has been combine into "Date"
-            dataset = dataset.drop(
-                [mergeDateTime[0], mergeDateTime[1]], axis=1)
-        except (KeyError):
-            print("Incorrect column name. Check",
-                  mergeDateTime[0], mergeDateTime[1])
-        except (IndexError):
-            print("Incorrect number of parameters in mergeDateTime")
-        except (ValueError):
-            print("Column can't be change to Datetime")
-
-    print("Date and Time merge finished...")  # ! Print
-
-    # Changes the data to types that use less memory
-    if nameFormat:
-        print("Values conversion started...")  # ! Print
-        for nameAndType in nameFormat:
-            try:
-                dataset = dataset.astype({nameAndType[0]: nameAndType[1]})
-            except (KeyError):
-                print("Incorrect column name:", nameAndType[0])
-            except (ValueError):
-                print(str(nameAndType[0]) +
-                      "can't be change to" + str(nameAndType[1]))
-
-    print("Values conversion finished...")  # ! Print
+    # Changes the data to types to use less memory
+    for nameAndType in columnAndType:
+        # Cast the column to the wanted type
+        dataset = dataset.astype({nameAndType[0]: nameAndType[1]})
+        # Interpolates the NaN values
+        dataset[nameAndType[0]] = dataset[nameAndType[0]].interpolate(
+            method="time", limit_direction='forward')
 
     return dataset
 
 
-def sampleDataset(dataset, columnFunction=[], frequency="15min"):
+def sampleDataset(dataset, columnAndFunction, frequency):
     """
     Sample dataset according to a time frequency given. The dataset 
     will be group in interval of `frequency` and a numeric function will
@@ -146,8 +117,8 @@ def sampleDataset(dataset, columnFunction=[], frequency="15min"):
     for each new timestamp.
 
     Different numeric functions can be applied to different column by 
-    pairing the function with the column in a tuple in `columnFunction`.
-    If a column is in the dataset but not in `columnFunction` or doesn't
+    pairing the function with the column in a tuple in `columnAndFunction`.
+    If a column is in the dataset but not in `columnAndFunction` or doesn't
     have a function assigned for the sampling, it will be ignore (and 
     removed) in the returned dataset.
 
@@ -155,15 +126,13 @@ def sampleDataset(dataset, columnFunction=[], frequency="15min"):
     ----------
     - dataset: pd.DataFrame.
         Dataset to sample each column.
-    - columnFunction: list of tuples.
+    - columnAndFunction: list of tuples.
         Each tuple is made of 2 parameters: 
         the name (string) of the column and the function to apply in
-        the sample. Wrong name or incorrect casting will result
-        in a failed conversion. By default is an empty list. Example: 
+        the sample. Example: 
         >>> [("Temp Out", "np.mean)]
     - frequency: string. Value expressed in string of the frequency to
-        sample the data. By default, the dataset gets sample in groups
-        of 15min. Other examples are:
+        sample the data. Examples:
         >>> "1H" --An hour
         >>> "1D" --A day
         >>> "1M" --A month
@@ -173,35 +142,21 @@ def sampleDataset(dataset, columnFunction=[], frequency="15min"):
     -------
     - dataset : pd.DataFrame.
         Dataset with the rows sample in the desired frequency.
-        If no sample could be made, return the original dataset and
-        a warning will be given.
     """
     # Creates the new dataset to return
     newDataset = pd.DataFrame()
 
-    print("Resampling started...")  # ! Print
+    for filterFunction in columnAndFunction:
+        # Generates a new DataFrame
+        # By concatenating the column to the new DataFrame
+        # As it gets sampled by the frequency
+        newDataset = pd.concat([newDataset,
+                                dataset.resample(frequency).agg(
+                                    {filterFunction[0]:filterFunction[1]})],
+                               axis=1)
 
-    for filterFunction in columnFunction:
-        try:
-            newDataset = pd.concat([newDataset,  # Concatenates to the right the result in the new dataset
-                                    dataset.resample(frequency).agg(  # Samples a specific column with its corresponding function
-                                        {filterFunction[0]:
-                                         filterFunction[1]})],
-                                   axis=1)
-        except (KeyError):
-            print("Incorrect column name:", filterFunction[0])
-        except (pandas.core.base.DataError):
-            print("Incorrect function to apply to:", filterFunction[0])
-
-    print("Resampling finished...")  # ! Print
-    print("Clearing NA values in resampled dataset...")  # ! Print
-
-    # Cleans the dataset from extra values generated while sampling
+    # TODO: preguntar si se deben borrar los NaN
+    # Clears the dataset from extra values generated while sampling
     newDataset = newDataset.dropna(axis=0, how="any")
-
-    # If it's empty, return the original dataset
-    if newDataset.empty:
-        warnings.warn("Returning un-sampled dataset", Warning)  # ! Warning
-        newDataset = dataset
 
     return newDataset
