2726923b78d41231bb02f51b8be0ebf27aa568f0
Andres Ramirez Quiros <andrew.rq+gitlab@gmail.com>
2020-08-17 19:21:33 -0600

    Unit tests modified to match new functions
    - Name of original test changed to match the present functions
    - New test added (mergeDateTime)

--- IsCoffeeWet/tests/test_dataParser.py
+++ IsCoffeeWet/tests/test_dataParser.py
@@ -8,48 +8,62 @@ class Test_TestDataParser(unittest.TestCase):
     def setUp(self):
         self.dirtyDataset = read_csv("resources/est0Corta.csv")
 
+    def test_mergeDateTime(self):
+        datetimeDS = dataParser.mergeDateTime(
+            self.dirtyDataset, "Date", "Time")
+
+        # New dataset must have less rows as 2 of them have been merged
+        self.assertNotEqual(len(datetimeDS.columns),
+                            len(self.dirtyDataset.columns))
+
     def test_cleanDataset(self):
-        testDS = dataParser.cleanDataset(self.dirtyDataset,
-                                         ["Temp Out", "Leaf Wet 1"],
-                                         "---")
-
-        self.assertLess(testDS.size, self.dirtyDataset.size)
-
-    def test_setDataTypes(self):
-        dataset = dataParser.cleanDataset(self.dirtyDataset,
-                                          ["Temp Out",
-                                           "Leaf Wet 1"],
+        clearDS = dataParser.cleanDataset(self.dirtyDataset,
+                                          ["Temp Out", "Leaf Wet 1"],
                                           "---")
-        testDS = dataParser.setDataTypes(dataset,
-                                         ("Date", "Time"),
-                                         [("Temp Out", "float32"),
-                                          ("Leaf Wet 1", "float32")])
 
-        self.assertLess(len(testDS.columns), len(dataset.columns))
+        # Checks the table for "---" aka nullvalue
+        # Uses any(0) to group all the rows in a value for each column
+        # Uses any(0) again to group the True and False into a single result
+        # If the result is false, all instances have been replaced
+        self.assertFalse(clearDS.eq("---").any(0).any(0))
+
+    def test_convertNumeric(self):
+        # INFO: Needs datetime index to interpolate by time
+        dataset = dataParser.mergeDateTime(self.dirtyDataset, "Date", "Time")
+        # Cleans the dataset of null values
+        dataset = dataParser.cleanDataset(dataset,
+                                          ["Temp Out", "Leaf Wet 1"],
+                                          "---")
+
+        convertDS = dataParser.convertNumeric(dataset,
+                                           [("Temp Out", "float32"),
+                                            ("Leaf Wet 1", "float32")])
+
+        # Checks the table for np.NaN
+        # Uses any(0) to group all the rows in a value for each column
+        # Uses any(0) again to group the True and False into a single result
+        # If the result is false, all instances have been replaced
+        self.assertFalse(convertDS.eq(np.NaN).any(0).any(0))
 
     def test_sampleDataset(self):
-        dataset = dataParser.cleanDataset(self.dirtyDataset,
-                                          ["Temp Out",
-                                           "Leaf Wet 1"],
+        # Merge into datetime and uses it as index
+        dataset = dataParser.mergeDateTime(self.dirtyDataset, "Date", "Time")
+        # Cleans the dataset of null values
+        dataset = dataParser.cleanDataset(dataset,
+                                          ["Temp Out", "Leaf Wet 1"],
                                           "---")
-        dataset = dataParser.setDataTypes(dataset,
-                                          ("Date", "Time"),
-                                          [("Temp Out", "float32"),
-                                           ("Leaf Wet 1", "float32")])
+        # Casts the dataset's columns into the needed dtypes
+        dataset = dataParser.convertNumeric(dataset,
+                                            [("Temp Out", "float32"),
+                                             ("Leaf Wet 1", "float32")])
 
-        # Check warning output
-        with self.subTest():
-            with self.assertWarns(Warning):
-                dataParser.sampleDataset(dataset)
-
-        with self.subTest():
-            # TODO: cambiar np.max de Leaf Wet 1
-            testDS = dataParser.sampleDataset(dataset,
-                                              [("Temp Out", np.mean),
-                                               ("Leaf Wet 1", np.max)],
-                                              "15min")
-            print(testDS.info(verbose=True))
-            self.assertLess(testDS.size, dataset.size)
+        # TODO: cambiar np.max de Leaf Wet 1
+        sampleDS = dataParser.sampleDataset(dataset,
+                                            [("Temp Out", np.mean),
+                                            ("Leaf Wet 1", np.max)],
+                                            "15min")
+        print(sampleDS.info(verbose=True))
+        self.assertLess(sampleDS.size, dataset.size)
 
 
 if __name__ == '__main__':
@@ -59,5 +73,5 @@ if __name__ == '__main__':
 #import pandas as pd
 #dataset = pd.read_csv("resources/est0Corta.csv")
 #dataset = dp.cleanDataset(dataset,["Temp Out", "Leaf Wet 1"],"---")
-#dataset = dp.setDataTypes(dataset,("Date","Time"),[("Temp Out","float32"),("Leaf Wet 1","int32")])
+#dataset = dp.convertNumeric(dataset,("Date","Time"),[("Temp Out","float32"),("Leaf Wet 1","int32")])
 #dataset = dp.sampleDataset(dataset, "1H")
