3e9f78ca514a5837518824f360fc6b3cc18278db
Andres Ramirez Quiros <andrew.rq+gitlab@gmail.com>
2020-08-27 19:18:30 -0600

    Change the dataset parsing from multiple inputs to a json

--- IsCoffeeWet/TopLevel.py
+++ IsCoffeeWet/TopLevel.py
@@ -1,6 +1,7 @@
 import matplotlib as plt
 import pandas as pd
 import numpy as np
+import json
 from IsCoffeeWet import dataParser as dp
 
 print("******************************************")
@@ -12,85 +13,63 @@ dataset = pd.read_csv(dataPath, engine="c")
 print("Dataset loaded... \n")
 
 isDataParsed = input(
-    "- Has your dataset been previously parsed?: (Yes/No) ")
+    "- Has your dataset been previously parsed? (Yes/No): ")
 
 if isDataParsed == "No":
     print("\n***Dataset parse started***")
 
-    date = input("- What's the column name with the dates?: ")
-    time = input("- What's the column name with the hours?: ")
-    dataset = dp.mergeDateTime(dataset, date, time)
-    print("Date & Time merge completed...\n")
+    # Remove trailing and leading spaces from column names
+    dataset.columns = dataset.columns.str.strip()
 
-    nullValue = input("- How your dataset manage null values?: ")
+    configPath = input("- Path to your config file (JSON): ")
+    with open(configPath, 'r') as file:
+        # Loads json file
+        configFile = json.load(file)
 
-    print("- Write the column name and the data type for it: ")
-    print("\t+ Separate using a coma (,)")
-    print("\t+ Data types available: signed, unsigned, float")
-    print("\t+ When finished, leave empty and press enter")
+        # Merge Date and Time into a single column
+        dataset = dp.mergeDateTime(
+            dataset, configFile["dateName"], configFile["timeName"])
 
-    columnAndType = []
-    # Stops the function when not enough inputs are received
-    while True:
-        try:
-            inputA, inputB = input("Column-name,data-type: ").split(",")
-            columnAndType.append((inputA, inputB))
-        except ValueError:
-            break
+        # List to construct
+        columnAndType = []
+        columnAndFunction = []
 
-    dataset = dp.convertNumeric(dataset, columnAndType, nullValue)
-    print("Conversion of columns to numbers completed...")
+        # Construct the list of tuples to use
+        for columnName in configFile["columns"]:
+            columnAndType.append(
+                (columnName, configFile["columnAndType"][columnName]))
+            columnAndFunction.append(
+                (columnName, configFile["columnAndFunction"][columnName]))
 
-    print("- What's the frequency to use for the dataset?")
-    print("Examples of correct inputs:")
-    print("\t15min")
-    print("\t1H (hour)")
-    print("\t1h30min")
-    print("\t1D (day)")
-    print("\t1M (month)")
-    frequency = input("freq: ")
+        dataset = dp.convertNumeric(
+            dataset, columnAndType, configFile["nullList"])
+        dataset = dp.sampleDataset(
+            dataset, columnAndFunction, configFile["frequency"])
 
-    print("- Write the column name and the function to apply during resampling: ")
-    print("\t+ Separate using a coma (,)")
-    print("\t+ Some available functions: mean, last, max, etc.")
-    print("\t+ When finished, leave empty and press enter")
+        # Fill empty values after sampling
+        dataset = dp.convertNumeric(
+            dataset, columnAndType, configFile["nullList"])
 
-    columnAndFunction = []
-    # Stops the function when not enough inputs are received
-    while True:
-        try:
-            inputA, inputB = input("Column-name,function: ").split(",")
-            columnAndFunction.append((inputA, inputB))
-        except ValueError:
-            break
+        dataset = dp.cyclicalEncoder(
+            dataset, configFile["encodeDays"], configFile["encodeHours"])
 
-    dataset = dp.sampleDataset(dataset, columnAndFunction, frequency)
-    print("Resampling completed...")
+        # Value used for filename of new dataset
+        if configFile["encodeDays"]:
+            encodeDays = "_encodedDays"
+        else:
+            encodeDays = ""
 
-    #Do again this function in case a sampling operation has turne
-    #integers in floats
-    dataset = dp.convertNumeric(dataset, columnAndType, nullValue)
+        # Value used for filename of new dataset
+        if configFile["encodeHours"]:
+            encodeHours = "_encodedHours"
+        else:
+            encodeHours = ""
 
-    encodeDays = input(
-        "- Do you want to encode days into a pair of sin/cos? (Yes/No) ")
-    encodeHours = input(
-        "- Do you want to encode hours into a pair of sin/cos? (Yes/No) ")
+        dataPath = dataPath.replace(".csv", "")
+        dataPath = dataPath + "_" + \
+            configFile["frequency"] + encodeDays + encodeHours + ".csv"
+        dataset.to_csv(dataPath)
+        print("A copy of your dataset has been save into: " + dataPath)
 
-    dataset = dp.cyclicalEncoder(
-        dataset, encodeDays == "Yes", encodeHours == "Yes")
-    print("Encoding complete completed...")
-
-    if encodeDays == "Yes":
-        encodeDays = "_encodedDays"
-    else:
-        encodeDays = ""
-
-    if encodeHours == "Yes":
-        encodeHours = "_encodedHours"
-    else:
-        encodeHours = ""
-
-    dataPath = dataPath.replace(".csv", "")
-    dataName = dataPath + "_" + frequency + encodeDays + encodeHours + ".csv"
-    dataset.to_csv(dataName)
-    print("A copy of your dataset has been save into: " + dataName)
+else:
+    pass
