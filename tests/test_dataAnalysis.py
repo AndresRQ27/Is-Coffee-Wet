dab432ffd107a53d76c101365af155dbc36a812e
Andrés Ramírez Quirós <andrew.rq+gitlab@gmail.com>
2020-09-10 19:46:14 -0600

    New test dataset

--- tests/test_dataAnalysis.py
+++ tests/test_dataAnalysis.py
@@ -6,7 +6,7 @@ from IsCoffeeWet import dataAnalysis
 class Test_TestDataAnalysis(unittest.TestCase):
     def setUp(self):
         # Sets the index using Datetime column
-        self.dataset = read_csv("resources/test_1H_encodedDays_encodedHours.csv",
+        self.dataset = read_csv("resources\test_1H_Day_Trimester.csv",
                                 engine="c", index_col="Datetime", parse_dates=True)
         # Infers the frequency
         self.dataset = self.dataset.asfreq(self.dataset.index.inferred_freq)
@@ -16,7 +16,7 @@ class Test_TestDataAnalysis(unittest.TestCase):
         self.assertTrue(True)
 
     def test_freqDomain(self):
-        dataAnalysis.freqDomain(self.dataset, ["Temp Out", "Leaf Wet 1"])
+        dataAnalysis.freqDomain(self.dataset, [("Temp Out"), "Leaf Wet 1"])
         self.assertTrue(True)
 
 
