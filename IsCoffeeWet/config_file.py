8dc87f105bf2c2bedcff720df934e916a39553ab
Andrés Ramírez Quirós <andrew.rq+gitlab@gmail.com>
2020-09-27 11:54:42 -0600

    New format for ConfigFile
    * Section general parameters from preprocess
    * Use of dictionaries
    * Default initializer

--- IsCoffeeWet/config_file.py
+++ IsCoffeeWet/config_file.py
@@ -1,57 +1,85 @@
 import json
+from pandas import read_csv
 
 
 class ConfigFile:
     """
-    Object that contains the values of the configuration file in the
-    JSON for the corresponding dataset. This helps to not have the file
-    open more than the necessary time, while retaining its data during 
-    the execution of the program.
+    Object that contains the values of the configuration file in the JSON
+    for the corresponding dataset. This helps to not have the file open
+    more than the necessary time, while retaining its data during the
+    execution of the program.
 
     Parameters
     ----------
-    - configPath : string.
-        Path of the JSON that has the configuration of the dataset to
-        use. Can be relative to the path where the program is running 
-        or absolute.
+    - config_path : string. Path of the JSON that has the configuration of
+        the dataset to use. Can be relative to the path where the program
+        is running or absolute.
     """
 
-    def __init__(self, configPath):
-        with open(configPath, 'r') as file:
-            # Loads json file
-            configFile = json.load(file)
+    def __init__(self, config_path = None):
+        # When initialize without arguments
+        if config_path == None:
+            return
 
-            # Name of the Date column
-            self.date = configFile["dateName"]
-            # Name of the Time column
-            self.time = configFile["timeName"]
+        # When initialize with a path
+        else:
+            with open(config_path, 'r') as file:
+                # Loads json file
+                config_file = json.load(file)
 
-            # List of the original columns
-            self.graph_columns = configFile["graphColumns"]
-            # List of the column names
-            self.columns = configFile["columns"]
-            # List of the type of each column
-            self.cType = []
-            # List of the function for each column
-            self.cFunction = []
+                # TODO: use a function to receive a folder path
+                # and return the most recent file in it
 
-            # Construct the list of tuples to use
-            for columnName in configFile["columns"]:
-                self.cType.append(
-                    (columnName, configFile["columnAndType"][columnName]))
-                self.cFunction.append(
-                    (columnName, configFile["columnAndFunction"][columnName]))
+                # Path of the file dataset to use
+                self.path = config_file["datasetPath"]
 
-            # List of strings to use as null
-            self.null = configFile["nullList"]
+                # Frequency of the dataset
+                self.freq = config_file["frequency"]
 
-            # Frequency of the dataset
-            self.freq = configFile["frequency"]
+                # Amount of days to forecast
+                self.forecast = config_file["forecastWindow"]
 
-            # List for time encoding
-            self.encode = []
+                # Boolean to graph the col
+                self.graph = config_file["graphData"]
 
-            # Construct the list of tuples to use
-            for id in range(len(configFile["encodeNames"])):
-                self.encode.append(
-                    (configFile["encodeNames"][id], configFile["encodeFreq"][id]))
+                # *****************************
+                # *** Preprocess parameters ***
+                # *****************************
+
+                # List of the columns to merge into datetime
+                self.datetime = config_file["preprocess"]["datetime"]
+
+                self.datetime_format = config_file["preprocess"]["datetime_format"]
+
+                # List of strings to use as null
+                self.null = config_file["nullList"]
+
+                # Read the list of columns to delete
+                deleteColumns = config_file["preprocess"]["deleteColumns"]
+
+                # List of columns to use
+                self.columns = read_csv(self.path,
+                                        engine="c",
+                                        nrows=1).columns.tolist()
+
+                # Removes the unwanted columns
+                for column_name in deleteColumns:
+                    self.columns.remove(column_name)
+
+                # Creates an empty dictionary for the column encoding
+                self.encode = {}
+                # Fill the dictionary
+                for name, time in config_file["preprocess"]["encodeTime"].items():
+                    self.encode[name] = time
+
+                # Creates an empty dictionary for the special functions
+                self.functions = {}
+                # Fill the dictionary
+                for name, functions in config_file["preprocess"]["specialFunctions"].items():
+                    self.functions[name] = functions
+
+                # Creates an empty dictionary for the special format
+                self.formats = {}
+                # Fill the dictionary
+                for name, formats in config_file["preprocess"]["specialFormat"].items():
+                    self.formats[name] = formats
